78011ee843305f0e02e916689b6d0c814d38c59e
Jaime <jaime.i.botas@gmail.com>
2020-09-26 17:03:44 +0100

    deployed on testnet

---
+++ README.md
@@ -0,0 +1,9 @@
+Generated Keyless Address = `0x1107Dd08bBeE561a2b571D0be982959503120c1e`
+Deterministic Contract Factory Address = `0x8BAd4FEDD5f45ea95781A9b292199DA5C410C8e6`
+ERC1820 Registry Address = `0x64fBCFd743E04cc62D52Bed5FdaC34eB2403A47b`
+
+[Deterministic Factory Goerli test-net](https://goerli.etherscan.io/tx/0x11a69dff54821c98f3eb11feba14129d456ce02d2bfb16701b8ec373daf55683)
+[ERC1820Registry Goerli test-net](https://goerli.etherscan.io/address/0x64fbcfd743e04cc62d52bed5fdac34eb2403a47b)
+
+[Deterministic Factory Rinkeby test-net](https://rinkeby.etherscan.io/tx/0x11a69dff54821c98f3eb11feba14129d456ce02d2bfb16701b8ec373daf55683)
+[ERC1820Registry Rinkeby test-net](https://rinkeby.etherscan.io/tx/https://rinkeby.etherscan.io/address/0x64fbcfd743e04cc62d52bed5fdac34eb2403a47b)
--- test/deployment.js
+++ test/deployment.js
@@ -7,8 +7,9 @@ var Tx = require("ethereumjs-tx").Transaction
 const { expect } = require('chai');
 
 contract('DeterministicContractFactory', function (accounts) {
+    console.log('hello')
     const gasPrice = 100000000000;
-    const deploymentGas = 999999;
+    const deploymentGas = 3000000;
     const deploymentBytecode = Factory.bytecode;
     
     const rawTx = {
@@ -31,19 +32,21 @@ contract('DeterministicContractFactory', function (accounts) {
     console.log('deterministicContractFactoryAddress', deterministicContractFactoryAddress);
 
     it('deploys', async function () {
-        await web3.eth.sendTransaction({ from: accounts[0], to: signerAddress, value: '100000000000000000' });
+        await web3.eth.sendTransaction({ from: accounts[0], to: signerAddress, value: '300000000000000000' });
     
         //console.log('balance', await web3.eth.getBalance(signerAddress));
     
         const res = await web3.eth.sendSignedTransaction(signedTx);
-        console.log('deployed factory address', res.contractAddress);
+        console.log('deployed factory address', res);
     
         const factoryContract = await Factory.at(deterministicContractFactoryAddress);
     
-        const magic = Buffer.from('Clearmatics.com', 'utf8');
+        const magic = Buffer.from('Coolbeans.com', 'utf8');
         const salt = bufferToHex(keccak256(magic));
     
-        await factoryContract.deploy(salt, ERC1820Registry.bytecode, { from : accounts[0] });
+        const receipt = await factoryContract.deploy(salt, ERC1820Registry.bytecode, { from : accounts[0] });
+        console.log('receipt', receipt);
+
         const ERC1820RegistryAddress = await factoryContract.computeAddress(salt, ERC1820Registry.bytecode, { from : accounts[0] });
 
         // check that the registry was deployed
