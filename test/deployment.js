6c28ed2091106f7ecb72be0145f8a21d12304b35
Jaime <jaime.i.botas@gmail.com>
2020-09-26 14:32:25 +0100

    almost donzo

--- test/deployment.js
+++ test/deployment.js
@@ -1,13 +1,15 @@
-const { accounts, contract, web3 } = require('@openzeppelin/test-environment');
+const Factory = artifacts.require('DeterministicContractFactory');
+const ERC1820Registry = artifacts.require('ERC1820Registry');
 
-const factory = contract.fromArtifact('DeterministicContractFactory');
-const { rlp, keccak256, ecrecover, pubToAddress, Address, bufferToHex, BN, toChecksumAddress, generateAddress, toBuffer  } = require('ethereumjs-util');
+const { bufferToHex, toChecksumAddress, generateAddress, toBuffer, keccak256  } = require('ethereumjs-util');
 var Tx = require("ethereumjs-tx").Transaction
 
-describe('DeterministicContractFactory', async function () {
+const { expect } = require('chai');
+
+contract('DeterministicContractFactory', function (accounts) {
     const gasPrice = 100000000000;
     const deploymentGas = 999999;
-    const deploymentBytecode = factory.bytecode;
+    const deploymentBytecode = Factory.bytecode;
     
     const rawTx = {
         nonce: 0,
@@ -16,8 +18,8 @@ describe('DeterministicContractFactory', async function () {
         value: 0,
         data: deploymentBytecode,
         v: 27,
-        r: '0x2222222222222222222222222222222222222222222222222222222222222222',
-        s: '0x2222222222222222222222222222222222222222222222222222222222222222'
+        r: '0x222222222222222222222222222222222222222222222222beefbeefbeefbeef',
+        s: '0x222222222222222222222222222222222222222222222222beefbeefbeefbeef'
     };
 
     const tx = new Tx(rawTx);
@@ -28,12 +30,28 @@ describe('DeterministicContractFactory', async function () {
     console.log('signerAddress', signerAddress);
     console.log('deterministicContractFactoryAddress', deterministicContractFactoryAddress);
 
-    const amount = web3.utils.toWei('100', 'ether');
+    it('deploys', async function () {
+        await web3.eth.sendTransaction({ from: accounts[0], to: signerAddress, value: '100000000000000000' });
+    
+        //console.log('balance', await web3.eth.getBalance(signerAddress));
+    
+        const res = await web3.eth.sendSignedTransaction(signedTx);
+        console.log('deployed factory address', res.contractAddress);
+    
+        const factoryContract = await Factory.at(deterministicContractFactoryAddress);
+    
+        const magic = Buffer.from('Clearmatics.com', 'utf8');
+        const salt = bufferToHex(keccak256(magic));
+    
+        await factoryContract.deploy(salt, ERC1820Registry.bytecode, { from : accounts[0] });
+        const ERC1820RegistryAddress = await factoryContract.computeAddress(salt, ERC1820Registry.bytecode, { from : accounts[0] });
 
-    await web3.eth.sendTransaction({ from: accounts[0], to: signerAddress, value: '100000000000000000' });
+        // check that the registry was deployed
+        const registry = await ERC1820Registry.at(ERC1820RegistryAddress);
+        const manager = await registry.getManager(accounts[0]);
 
-    console.log('balance', await web3.eth.getBalance(signerAddress));
+        expect(manager).to.be.equal(accounts[0]);
 
-    const res = await web3.eth.sendSignedTransaction(signedTx);
-    console.log('deployed', res.contractAddress);
+        console.log('ERC1820RegistryAddress', ERC1820RegistryAddress)
+    });
 });
